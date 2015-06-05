73cbad72f093d8f81df16f8aea97b853e398de65
Ben Youngblood <bx.youngblood@gmail.com>
2015-06-05 10:49:14 -0700

    examples, plus working thermostat setpoint command

--- cmd/gatewayd/main.go
+++ cmd/gatewayd/main.go
@@ -23,15 +23,87 @@ func main() {
 	serialPort.Initialize()
 	go serialPort.Run()
 
-	go func() {
-		version := serialPort.SendFrameSync(zwave.NewRequestFrame([]byte{0x15}))
-		fmt.Println(version)
-	}()
+	// MAIN: &{1 26 0 [74 1 3 0 19 4 8 6 64 66 67 68 69 128 112 49 143 134 114 133 44 43 115 129] 62}
+	// funcid = 1
+	// status = 3
+	// source = 0
+	// blen = 19
+	//
+
+	// serialPort.SendFrame(zwave.NewRequestFrame(zwave.GetNodeList()), func(status int) {
+	// 	fmt.Println("STATUS:", status)
+	// })
+
+	f := zwave.NewRequestFrame(zwave.SendData(uint8(29), zwave.NewThermostatSetpointCommand()))
+	go serialPort.SendFrame(f, func(status int) {
+		fmt.Println("STATUS:", status)
+	})
+
+	// go serialPort.SendFrame(zwave.NewRequestFrame(zwave.EnterInclusionMode()), func(status int) {
+	// 	if status == int(zwave.FrameHeaderAck) {
+	// 		fmt.Println("Entered inclusion mode")
+	// 	} else {
+	// 		fmt.Println("Could not enter inclusion mode")
+	// 	}
+	// })
+
+	// go serialPort.SendFrame(zwave.NewRequestFrame(zwave.ExitInclusionMode()), func(status int) {
+	// 	if status == int(zwave.FrameHeaderAck) {
+	// 		fmt.Println("Exited inclusion mode")
+	// 	} else {
+	// 		fmt.Println("Could not exit inclusion mode")
+	// 	}
+	// })
+
+	// go serialPort.SendFrame(zwave.NewRequestFrame(zwave.EnterExclusionMode()), func(status int) {
+	// 	if status == int(zwave.FrameHeaderAck) {
+	// 		fmt.Println("Entered exclusion mode")
+	// 	} else {
+	// 		fmt.Println("Could not enter exclusion mode")
+	// 	}
+	// })
+
+	// go serialPort.SendFrame(zwave.NewRequestFrame(zwave.ExitExclusionMode()), func(status int) {
+	// 	if status == int(zwave.FrameHeaderAck) {
+	// 		fmt.Println("Exited exclusion mode")
+	// 	} else {
+	// 		fmt.Println("Could not exit exclusion mode")
+	// 	}
+	// })
 
 	for {
-		// frame := <-serialPort.Incoming
+		frame := <-serialPort.Incoming
 		// packet := common.WirePacket{frame.Marshal()}
-		fmt.Println("MAIN:", <-serialPort.Incoming)
+		fmt.Println("MAIN:", frame)
+
+		if frame.Payload[0] == 0x02 && frame.IsResponse() {
+			fmt.Println("Frame payload contains a node list")
+			nodeList := zwave.NodeList{}
+			nodeList.Unmarshal(frame)
+			fmt.Println(nodeList.GetNodeIds())
+		}
+
+		if frame.Payload[0] == 0x4a && frame.Payload[2] == 5 {
+			fmt.Println("NODE ADDED")
+			go serialPort.SendFrame(zwave.NewRequestFrame(zwave.ExitInclusionMode()), func(status int) {
+				if status == int(zwave.FrameHeaderAck) {
+					fmt.Println("Exited inclusion mode")
+				} else {
+					fmt.Println("Could not exit inclusion mode")
+				}
+			})
+		}
+
+		if frame.Payload[0] == 0x4b && frame.Payload[2] == 5 {
+			fmt.Println("NODE REMOVED")
+			go serialPort.SendFrame(zwave.NewRequestFrame(zwave.ExitExclusionMode()), func(status int) {
+				if status == int(zwave.FrameHeaderAck) {
+					fmt.Println("Exited exclusion mode")
+				} else {
+					fmt.Println("Could not exit exclusion mode")
+				}
+			})
+		}
 	}
 
 	// defer serialPort.Close()
