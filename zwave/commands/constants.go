ddac65778f0b69ebd61dae9c48b22187d540ddd6
Ben Youngblood <bx.youngblood@gmail.com>
2015-06-09 12:30:09 -0700

    add some new constants for the security frame

--- zwave/commands/constants.go
+++ zwave/commands/constants.go
@@ -3,11 +3,25 @@ package commands
 const (
 	CommandClassDoorLock             = 0x62
 	CommandClassThermostatSetpointV3 = 0x43
+	CommandClassSecurity             = 0x98
 )
 
 const (
-	DoorLockOperationSet  = 0x01
+	// Door lock
+	DoorLockOperationSet = 0x01
+
+	// Thermostat
 	ThermostatSetpointSet = 0x01
+
+	// Security
+	NetworkKeySet                        = 0x06
+	NetworkKeyVerify                     = 0x07
+	SecurityCommandsSupportedGet         = 0x02
+	SecurityMessageEncapsulation         = 0x81
+	SecurityMessageEncapsulationNonceGet = 0xC1
+	SecurityNonceGet                     = 0x40
+	SecurityNonceReport                  = 0x80
+	SecuritySchemeGet                    = 0x04
 )
 
 const (
