f0fb55841d325c311b289636627d1ac374aebdeb
Ben Youngblood <bx.youngblood@gmail.com>
2015-07-02 07:49:02 -0700

    add some command class definitions

--- zwave/commandclass/command-classes.go
+++ zwave/commandclass/command-classes.go
@@ -1,6 +1,15 @@
 package commandclass
 
 const (
-	CommandClassBasic            uint8 = 0x20
-	CommandClassSwitchMultiLevel       = 0x26
+	CommandClassBasic                   uint8 = 0x20
+	CommandClassSwitchMultiLevel              = 0x26
+	CommandClassSwitchAll                     = 0x27
+	CommandClassSceneActivation               = 0x2B
+	CommandClassSceneActuatorConf             = 0x2C
+	CommandClassManufacturerSpecific          = 0x72
+	CommandClassPowerLevel                    = 0x73
+	CommandClassNodeNaming                    = 0x77
+	CommandClassVersion                       = 0x86
+	CommandClassManufacturerProprietary       = 0x91
+	CommandClassSecurity                      = 0x98
 )
