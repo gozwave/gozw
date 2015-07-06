85d128ab4ca0fa062661031b03a8cb53a3518394
Ben Youngblood <bx.youngblood@gmail.com>
2015-07-06 13:51:13 -0700

    add constants related to association command class

---
+++ zwave/commandclass/association.go
@@ -0,0 +1,13 @@
+package commandclass
+
+const (
+	AssociationVersion             uint8 = 0x02
+	AssociationGet                       = 0x02
+	AssociationGroupingsGet              = 0x05
+	AssociationGroupingsReport           = 0x06
+	AssociationRemove                    = 0x04
+	AssociationReport                    = 0x03
+	AssociationSet                       = 0x01
+	AssociationSpecificGroupGet          = 0x0B
+	AssociationSpecificGroupReport       = 0x0C
+)
