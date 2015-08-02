95cf8babc3f7b3c9fc4858ee95de39a33771ba93
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-02 14:08:30 -0700

    Clean up test cli "repl"

--- cmd/test.go
+++ cmd/test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/bjyoungblood/gozw/zwave/application"
 	"github.com/bjyoungblood/gozw/zwave/frame"
@@ -27,65 +28,41 @@ func main() {
 		panic(err)
 	}
 
-	// spew.Dump(applicationLayer.Nodes)
-
-	// apiLayer.SoftReset()
-
-	// spew.Dump(apiLayer.GetVersion())
-	// nodeList, err := apiLayer.GetNodeList()
-	// fmt.Println(nodeList.GetNodeIds())
-	// spew.Dump(apiLayer.MemoryGetId())
-	// spew.Dump(apiLayer.GetNodeProtocolInfo(27))
-	// spew.Dump(apiLayer.GetSerialApiCapabilities())
-
-	// txTime, err := apiLayer.SendData(27, commandclass.NewVersionGet())
-	// fmt.Println("TX: ", txTime)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// if apiLayer.AddNode() != nil {
-	// 	apiLayer.AddNode()
-	// }
-
-	// sessionLayer := zwave.NewSessionLayer(frameLayer)
-	// manager := zwave.NewManager(sessionLayer)
-
-	// defer manager.Close()
-
-	fmt.Printf("Home ID: 0x%x; Node ID: %d\n", appLayer.HomeId, appLayer.NodeId)
-	fmt.Println("API Version:", appLayer.ApiVersion)
-	fmt.Println("Library:", appLayer.ApiLibraryType)
-	fmt.Println("Version:", appLayer.Version)
-	fmt.Println("API Type:", appLayer.ApiType)
-	fmt.Println("Is Primary Controller:", appLayer.IsPrimaryController)
-	fmt.Println("Node count:", len(appLayer.Nodes()))
-	//
-	// appLayer.SendDataSecure(42, []byte{
-	// 	commandclass.CommandClassDoorLock,
-	// 	0x01, // door lock operation set
-	// 	0xFF, // unsecured
-	// })
-
-	// manager.SetApplicationNodeInformation()
-	// manager.FactoryReset()
-
-	for _, node := range appLayer.Nodes() {
-		fmt.Println(node.String())
-	}
-
-	// manager.SendData(3, cc.NewSwitchMultilevelCommand(0))
+	defer appLayer.Shutdown()
 
 	line := liner.NewLiner()
 	defer line.Close()
 
+	commands := strings.Join([]string{
+		"(a)dd node",
+		"(r)emove node",
+		"(V) load command class versions for node",
+		"(L) load all user codes for node",
+		"(NIF) request node information frame from node",
+		"(F)ailed node removal",
+		"(p)rint network info",
+		"(q)uit",
+	}, "\n")
+
+	fmt.Println(commands)
+
 	for {
-		cmd, _ := line.Prompt("(a)dd node\n(r)emove node\n(g)et nonce\n(q)uit\n> ")
+		cmd, _ := line.Prompt("> ")
 		switch cmd {
 		case "a":
 			spew.Dump(appLayer.AddNode())
 		case "r":
 			spew.Dump(appLayer.RemoveNode())
+		case "V":
+			input, _ := line.Prompt("node id: ")
+			nodeId, _ := strconv.Atoi(input)
+			node, err := appLayer.Node(byte(nodeId))
+			if err != nil {
+				spew.Dump(err)
+				continue
+			}
+
+			spew.Dump(node.LoadCommandClassVersions())
 		case "L":
 			input, _ := line.Prompt("node id: ")
 			nodeId, _ := strconv.Atoi(input)
@@ -96,27 +73,32 @@ func main() {
 			}
 
 			spew.Dump(node.LoadAllUserCodes())
+		case "NIF":
+			input, _ := line.Prompt("node id: ")
+			nodeId, _ := strconv.Atoi(input)
+			node, _ := appLayer.Node(byte(nodeId))
+			spew.Dump(node.RequestNodeInformationFrame())
 		case "F":
 			input, _ := line.Prompt("node id: ")
 			nodeId, _ := strconv.Atoi(input)
 			spew.Dump(appLayer.RemoveFailedNode(byte(nodeId)))
-		// case "s":
-		// 	input, _ := line.Prompt("node id: ")
-		// 	nodeId, _ := strconv.Atoi(input)
-		// 	manager.SendData(uint8(nodeId), commandclass.NewSecuritySchemeGet())
-		// case "g":
-		// 	input, _ := line.Prompt("node id: ")
-		// 	nodeId, _ := strconv.Atoi(input)
-		// 	manager.SendData(uint8(nodeId), commandclass.NewSecurityNonceGet())
-		// case "v":
-		// 	input, _ := line.Prompt("node id: ")
-		// 	nodeId, _ := strconv.Atoi(input)
-		// 	manager.SendData(uint8(nodeId), commandclass.NewVersionGet())
+		case "p":
+			fmt.Printf("Home ID: 0x%x; Node ID: %d\n", appLayer.HomeId, appLayer.NodeId)
+			fmt.Println("API Version:", appLayer.ApiVersion)
+			fmt.Println("Library:", appLayer.ApiLibraryType)
+			fmt.Println("Version:", appLayer.Version)
+			fmt.Println("API Type:", appLayer.ApiType)
+			fmt.Println("Is Primary Controller:", appLayer.IsPrimaryController)
+			fmt.Println("Node count:", len(appLayer.Nodes()))
+
+			for _, node := range appLayer.Nodes() {
+				fmt.Println(node.String())
+			}
 		case "q":
-			appLayer.Shutdown()
 			return
 		default:
-			fmt.Println("invalid selection")
+			fmt.Println("invalid selection\n")
+			fmt.Println(commands)
 		}
 	}
 
