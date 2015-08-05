a691a15df864a108440ff27dd065eb87158ddf4c
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-04 18:03:55 -0700

    remove old code

--- zwave/constants.go
+++
@@ -1,9 +0,0 @@
-package zwave
-
-const (
-	ApplicationNodeInfoNotListening          = 0x00
-	ApplicationNodeInfoListening             = 0x01
-	ApplicationNodeInfoOptionalFunctionality = 0x02
-	ApplicationFreqListeningMode1000ms       = 0x10
-	ApplicationFreqListeningMode250ms        = 0x20
-)
