cb20069684bee8c77ab9c9a2470eb099de67fcf0
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-07 14:48:33 -0700

    Some code generation stuff
    I was thinking about using this for command class parsing, but I
    think that all of the different types (BYTE_STRUCT in particular)
    are going to end up making it more complicated than it's really
    worth, and we really don't need to support *so* many command classes
    that it's really worth the time it's going to take to figure out how
    to format everything.
    
    I committed the XML definitions just in case (not a terrible reference
    if you want to verify at the mask/shift values for a particular CC).

---
+++ ccgen/helpers.go
@@ -0,0 +1,67 @@
+package ccgen
+
+import (
+	"strings"
+
+	"github.com/aymerick/raymond"
+	"github.com/reiver/go-stringcase"
+)
+
+func init() {
+	raymond.RegisterHelpers(map[string]interface{}{
+		"toPascalCase":  stringcase.ToPascalCase,
+		"toPackageName": toPackageName,
+		"getGoType":     getGoType,
+		"notZeroByte":   notZeroByte,
+	})
+}
+
+func getGoType(paramType string) string {
+	switch paramType {
+	case "ARRAY":
+		return "[]byte"
+	case "BIT_24":
+		panic("Unimplemented param type: BIT_24")
+	case "BITMASK":
+		return "byte"
+	case "BYTE":
+		return "byte"
+	case "CONST":
+		return "byte"
+	case "DWORD":
+		return "uint32"
+	case "ENUM":
+		return "byte"
+	case "ENUM_ARRAY":
+		return "[]byte"
+	case "MARKER":
+		panic("Unimplemented param type: MARKER")
+	case "MULTI_ARRAY":
+		panic("Unimplemented param type: MULTI_ARRAY")
+	case "STRUCT_BYTE":
+		panic("Unimplemented param type: STRUCT_BYTE")
+	case "VARIANT":
+		return "[]byte"
+	case "WORD":
+		return "uint16"
+	default:
+		panic("Unknown param type: " + paramType)
+	}
+}
+
+// func notZeroByte(opts *raymond.Options) bool {
+// 	str := opts.ParamStr(0)
+// 	return str == "0x00"
+// }
+
+func notZeroByte(str string, options *raymond.Options) string {
+	if str != "0x00" {
+		return options.Fn()
+	}
+	return ""
+}
+
+func toPackageName(ccname string) string {
+	ccname = strings.Replace(ccname, "COMMAND_CLASS_", "", 1)
+	return stringcase.ToLowerCase(stringcase.ToPascalCase(ccname))
+}
---
+++ cmd/ccgen/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bjyoungblood/gozw/ccgen"
+)
+
+func main() {
+
+	gen, err := ccgen.NewGenerator()
+	if err != nil {
+		panic(err)
+	}
+
+	// devices, err := gen.GenDevices()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(devices)
+
+	cc, err := gen.GenCommandClasses()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cc)
+}
