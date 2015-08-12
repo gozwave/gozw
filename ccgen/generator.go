a3ffc96e498c8253d521b89e05ac584d76fedf1b
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-12 10:30:24 -0700

    Put each command class command in a separate file

--- ccgen/generator.go
+++ ccgen/generator.go
@@ -63,44 +63,65 @@ func (g *Generator) GenCommandClasses() error {
 			continue
 		}
 
-		buf := bytes.NewBuffer([]byte{})
-		err := g.tpl.ExecuteTemplate(buf, "commandClass.tpl", cc)
-		if err != nil {
-			return err
-		}
-
 		dirName := "zwave/command-class/" + cc.GetDirName()
-		filename := dirName + "/" + cc.GetDirName() + ".go"
-		os.Mkdir(dirName, 0775)
-		fp, err := os.Create(filename)
-		if err != nil {
+		err := os.Mkdir(dirName, 0775)
+		if err != nil && !strings.HasSuffix(err.Error(), "file exists") {
 			return err
 		}
 
-		formatted, err := format.Source(buf.Bytes())
-		if err != nil {
-			fmt.Println(string(buf.Bytes()))
-			fmt.Println(cc.Name)
-			return err
+		for _, cmd := range cc.Commands {
+			err := g.generateCommand(dirName, cc, cmd)
+			if err != nil {
+				return err
+			}
 		}
 
-		imported, err := imports.Process(filename, formatted, nil)
-		if err != nil {
-			fmt.Println(cc.Name)
-			return err
-		}
-
-		fp.Write(imported)
-		fp.Close()
 	}
 
 	if len(skipped) > 0 {
 		fmt.Println("Skipped generation for the following command classes:")
 		for _, cc := range skipped {
 			_, reason := cc.CanGenerate()
-			fmt.Printf("  - %s\n      Reason: %s\n", cc.Name, reason)
+			fmt.Printf("  - %s (%s)\n", cc.Name, reason)
 		}
 	}
+
+	return nil
+}
+
+func (g *Generator) generateCommand(dirName string, cc CommandClass, cmd Command) error {
+	buf := bytes.NewBuffer([]byte{})
+
+	err := g.tpl.ExecuteTemplate(buf, "command.tpl", map[string]interface{}{
+		"CommandClass": cc,
+		"Command":      cmd,
+	})
+	if err != nil {
+		return err
+	}
+
+	filename := dirName + "/" + cmd.GetFileName(cc) + ".gen.go"
+	fp, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	defer fp.Close()
+
+	formatted, err := format.Source(buf.Bytes())
+	if err != nil {
+		fmt.Println(string(buf.Bytes()))
+		fmt.Println(cc.Name)
+		return err
+	}
+
+	imported, err := imports.Process(filename, formatted, nil)
+	if err != nil {
+		fmt.Println(cc.Name)
+		return err
+	}
+
+	fp.Write(imported)
 
 	return nil
 }
