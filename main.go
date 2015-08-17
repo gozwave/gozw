9c7923bfab6789244738589bee6207d724e51e77
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-16 17:20:52 -0700

    add root files

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/helioslabs/ccgen/ccgen"
+
+func main() {
+
+	gen, err := ccgen.NewGenerator()
+	if err != nil {
+		panic(err)
+	}
+
+	err = gen.GenDevices()
+	if err != nil {
+		panic(err)
+	}
+
+	err = gen.GenCommandClasses()
+	if err != nil {
+		panic(err)
+	}
+
+	err = gen.GenParser()
+	if err != nil {
+		panic(err)
+	}
+
+}
