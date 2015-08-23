31900b2fa3ca77bd60da5048ab4842714dda2d1f
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-23 11:16:52 -0700

    update zwgen usage

--- zwave/command-class/command-classes.go
+++ zwave/command-class/command-classes.go
@@ -2,7 +2,9 @@ package commandclass
 
 import "encoding"
 
-//go:generate zwgen -o .
+//go:generate zwgen command-classes -c zwgen.config.yaml -o .
+//go:generate zwgen parser -c zwgen.config.yaml -o ./command-classes.gen.go
+//go:generate zwgen devices -c zwgen.config.yaml -o ./devices.gen.go
 
 type Command interface {
 	encoding.BinaryMarshaler
