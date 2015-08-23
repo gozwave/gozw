af162951571367d17d17f9a6ab1b4026d18251f3
Ben Youngblood <bx.youngblood@gmail.com>
2015-08-23 10:15:33 -0700

    Fix *the* bug with security
    Why do I keep introducing bugs with security?

--- zwave/security/encrypted-message.go
+++ zwave/security/encrypted-message.go
@@ -25,17 +25,17 @@ func (cmd EncryptedMessage) CommandID() byte {
 func (cmd *EncryptedMessage) UnmarshalBinary(data []byte) error {
 	// According to the docs, we must copy data if we wish to retain it after returning
 
-	if len(data) < 17 {
+	if len(data) < 19 {
 		return errors.New("Payload length underflow")
 	}
 
 	payload := make([]byte, len(data))
 	copy(payload, data)
 
-	cmd.SenderNonce = payload[0:8]
-	cmd.EncryptedPayload = payload[8 : len(payload)-9]
-	cmd.ReceiverNonceID = payload[8+len(cmd.EncryptedPayload)]
-	cmd.HMAC = payload[9+len(cmd.EncryptedPayload):]
+	cmd.SenderNonce = payload[2:10]
+	cmd.EncryptedPayload = payload[10 : len(payload)-9]
+	cmd.ReceiverNonceID = payload[10+len(cmd.EncryptedPayload)]
+	cmd.HMAC = payload[11+len(cmd.EncryptedPayload):]
 
 	return nil
 }
@@ -43,6 +43,8 @@ func (cmd *EncryptedMessage) UnmarshalBinary(data []byte) error {
 func (cmd *EncryptedMessage) MarshalBinary() (payload []byte, err error) {
 	payload = make([]byte, 0)
 
+	payload = append(payload, cmd.CommandClassID())
+	payload = append(payload, cmd.CommandID())
 	payload = append(payload, cmd.SenderNonce...)
 	payload = append(payload, cmd.EncryptedPayload...)
 	payload = append(payload, cmd.ReceiverNonceID)
